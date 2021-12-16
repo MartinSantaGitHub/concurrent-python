0e622bad703b24a6dae190425984f4fb115cd4b7
martins <martin.santamaria@rga.com>
2021-12-16 09:15:29 -0300

    Comments in main_process

--- src/02-sequencer/main_process.py
+++ src/02-sequencer/main_process.py
@@ -16,6 +16,7 @@ def show_id(db_conn: DatabaseConnectionBase):
     current_id = db_conn.get_id()
 
     print_results(current_process.pid, thread_id, current_id)
+    #print(f'db_conn: {str(db_conn)}')
 
 
 def show_id_safe(db_conn: DatabaseConnectionBase, semaphore: Semaphore):
@@ -26,6 +27,7 @@ def show_id_safe(db_conn: DatabaseConnectionBase, semaphore: Semaphore):
         current_id = db_conn.get_id()
 
     print_results(current_process.pid, thread_id, current_id)
+    #print(f'db_conn: {str(db_conn)}')
 
 
 def print_results(pid: int, thread_id: int, current_id: int):
@@ -45,14 +47,13 @@ def main():
     processes = []
 
     print()
+    #print(f'db_conn init: {str(db_conn)}')
 
     for _ in range(6):
-        processes.append(Process(target=show_id, args=(db_conn,)))
+        processes.append(Process(target=show_id_safe, args=(db_conn, semaphore)))
 
     [p.start() for p in processes]
     [p.join() for p in processes]
-
-    print()
 
 
 if __name__ == '__main__':
